61812b183a4bca4291aef075a561d122ec814d76
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-14 21:13:26 -0600

    funcion max

--- funciones.py
+++ funciones.py
@@ -5,3 +5,10 @@ if x>y:
 else:
     maximo=y
 print("el máximo entre ",x,"y",y,"es",maximo)   
+
+def max(x,y)
+""" Esta funcion calcula el maximo entre dos numros"""
+    if x>y:
+        maximo=x
+    else:
+        maximo=y
