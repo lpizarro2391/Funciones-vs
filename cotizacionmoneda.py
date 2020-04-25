d1797b73bf4cf8f1be534ceb22fa067e598890fb
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-25 13:23:22 -0600

    cotizacion de moneda solucion

--- cotizacionmoneda.py
+++ cotizacionmoneda.py
@@ -1,19 +1,3 @@
-i=0
-total=0
-while (i < 3):
-    cripto = input("Ingrese el nombre de la moneda: ")
-    if esmoneda():
-        i = i + 1
-        cant= " "
-        while not esnumero(cant):
-            cant = input("Indique la cantidad de "+cripto+":")
-        cotiz=""
-        while not esnumero(cotiz):
-            cotiz = input("Indique la cantidad de "+cripto+":")
-        total = total + float(cant) * float (cotiz) 
-    else:
-        print("Moneda Inválida.")
-print("El total en USD que tiene el usuario es de ",str(total))
 def esmoneda(cripto):
     criptos = ["BTC","BCC","LTC","ETH","ETC","XRP"]
     if cripto in criptos:
@@ -22,3 +6,19 @@ def esmoneda(cripto):
         return False
 def esnumero(numero):
     return numero.replace('.','',1).isdigit()
+i=0
+total=0
+while (i < 3):
+    cripto = input("Ingrese el nombre de la moneda: ")
+    if esmoneda(cripto):
+        i = i + 1
+        cant= " "
+        while not esnumero(cant):
+            cant = input("Indique la cantidad de "+cripto+":")
+        cotiz=""
+        while not esnumero(cotiz):
+            cotiz = input("Indique la Cotizacion de "+cripto+":")
+        total = total + float(cant) * float (cotiz) 
+    else:
+        print("Moneda Inválida.")
+print("El total en USD que tiene el usuario es de ",str(total))
