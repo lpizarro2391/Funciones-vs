65ae89296cb8d31b22ade1c9ac579e7171f14685
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-26 20:35:06 -0600

    Supongamos que no desea verificar el código de estado de la respuesta en una declaración if. En cambio, desea generar una excepción si la solicitud no tuvo éxito. Puedes hacer esto usando .raise_for_status ():

--- import requests.py
+++ import requests.py
@@ -1,10 +1,15 @@
 import requests
-response = requests.get('https://api.github.com')
-if response.status_code == 200:
-    print('Success!')
-elif response.status_code == 404:
-    print('Not Found.')
-if response:
-    print('Success!')
-else:
-    print('An error has occurred.')
+from requests.exceptions import HTTPError
+
+for url in ['https://api.github.com', 'https://api.github.com/invalid']:
+    try:
+        response = requests.get(url)
+
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        print('Success!')
