8403afdefd65c11c3e3a51d6263a11dd153d1855
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-26 20:57:13 -0600

    Si la solicitud establece una conexión dentro de 2 segundos y recibe datos dentro de los 5 segundos posteriores a la conexión establecida, la respuesta se devolverá como estaba antes. Si la solicitud agota el tiempo de espera, la función generará una excepción de tiempo de espera:

--- import requests.py
+++ import requests.py
@@ -1,2 +1,9 @@
 import requests
-requests.get('https://api.github.com', timeout=(2, 5))
+from requests.exceptions import Timeout
+
+try:
+    response = requests.get('https://api.github.com', timeout=1)
+except Timeout:
+    print('The request timed out')
+else:
+    print('The request did not time out')
