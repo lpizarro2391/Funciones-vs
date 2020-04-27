db4a751ef621b664311fcd44da21f7b4f7fab169
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-26 20:54:15 -0600

    Cada vez que los datos que intenta enviar o recibir son confidenciales, la seguridad es importante. La forma en que se comunica con sitios seguros a través de HTTP es estableciendo una conexión encriptada usando SSL, lo que significa que verificar el Certificado SSL del servidor de destino es crítico.
    La buena noticia es que las solicitudes lo hacen por usted de manera predeterminada. Sin embargo, hay algunos casos en los que es posible que desee cambiar este comportamiento.
    
    Si desea deshabilitar la verificación del Certificado SSL, pase Falso al parámetro de verificación de la función de solicitud:

--- import requests.py
+++ import requests.py
@@ -1,7 +1,4 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from getpass import getpass
-requests.get(
-    'https://api.github.com/user',
-    auth=HTTPBasicAuth('username', getpass())
-)
+requests.get('https://api.github.com', verify=False)
