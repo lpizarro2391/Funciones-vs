d869160ec4dcac0646063ec7015ce6f5cb2a1c95
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-26 20:33:15 -0600

    estado de solicitud

--- import requests.py
+++ import requests.py
@@ -4,3 +4,7 @@ if response.status_code == 200:
     print('Success!')
 elif response.status_code == 404:
     print('Not Found.')
+if response:
+    print('Success!')
+else:
+    print('An error has occurred.')
