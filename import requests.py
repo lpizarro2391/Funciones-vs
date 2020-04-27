5b81b31a267743cf32937f6d492e8505c93dd018
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-26 20:52:04 -0600

    Por lo tanto, puede realizar la misma solicitud pasando credenciales de autenticación básicas explícitas utilizando HTTPBasicAuth:

--- import requests.py
+++ import requests.py
@@ -1,3 +1,7 @@
 import requests
+from requests.auth import HTTPBasicAuth
 from getpass import getpass
-requests.get('https://api.github.com/user', auth=('username', getpass()))
+requests.get(
+    'https://api.github.com/user',
+    auth=HTTPBasicAuth('username', getpass())
+)
