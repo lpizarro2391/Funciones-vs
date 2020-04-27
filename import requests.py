898828bb6ffa62abf7cf62b23e721f08df6408ee
lpizarro2391 <lourdespizarro@gmail.com>
2020-04-26 20:49:35 -0600

    Autenticación La autenticación ayuda a un servicio a comprender quién es usted. Por lo general, usted proporciona sus credenciales a un servidor al pasar datos a través del encabezado de Autorización o un encabezado personalizado definido por el servicio. Todas las funciones de solicitud que ha visto hasta este momento proporcionan un parámetro llamado auth, que le permite pasar sus credenciales.
    Un ejemplo de una API que requiere autenticación es la API de usuario autenticado de GitHub. Este punto final proporciona información sobre el perfil del usuario autenticado. Para realizar una solicitud a la API de usuario autenticado, puede pasar su nombre de usuario y contraseña de GitHub en una tupla para obtener ():

--- import requests.py
+++ import requests.py
@@ -1,13 +1,3 @@
 import requests
-
-# Search GitHub's repositories for requests
-response = requests.get(
-    'https://api.github.com/search/repositories',
-    params={'q': 'requests+language:python'},
-)
-
-# Inspect some attributes of the `requests` repository
-json_response = response.json()
-repository = json_response['items'][0]
-print(f'Repository name: {repository["name"]}')  # Python 3.6+
-print(f'Repository description: {repository["description"]}')  # Python 3.6+
+from getpass import getpass
+requests.get('https://api.github.com/user', auth=('username', getpass()))
